ec6854bfa7aa9e2f8a391e5399e99dc4f282cd28
Kardo Heydari <120682793+imkardo@users.noreply.github.com>
2023-09-15 17:59:26 +0330

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -11,6 +11,8 @@ const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+let scores, currentScore, activePlayer, playing;
+
 
 // Starting conditions
 const init = function () {
